d5d37a6fb5653ad1cf5f1addb915a72c95b04489
Michael Becco <e4e6d4d5@gmail.com>
2019-05-13 17:09:47 -0700

    Test Conflight w/ Bcrypt NB: Return to Later

---
+++ test/system/users/user_sign_in_test.rb
@@ -0,0 +1,11 @@
+require "application_system_test_case"
+
+class UserSignInTest < ApplicationSystemTestCase
+  # System Test Error: BCrypt::Errors::InvalidHash: invalid hash
+  # test 'valid credential user logs in' do
+  #   visit user_session_path
+  #   fill_in 'user[email]', with: 'vic@theshield.net'
+  #   fill_in 'user[password]', with: 'password'
+  #   find('input[name="commit"]').click
+  # end
+end
