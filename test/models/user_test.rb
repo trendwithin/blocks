9b93c5ed89a76ac69031c33f040051a612a4d366
Michael Becco <e4e6d4d5@gmail.com>
2019-05-14 19:29:41 -0700

    Test User Pin Count

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,11 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'user pin count' do
+    user = users(:user_vic)
+    count = 2
+    pin_count = user.pins.count
+    assert_equal count, pin_count
+  end
 end
