6cc2c9fa8c984c5d90f58762acbcf1bc34baee4a
Michael Becco <e4e6d4d5@gmail.com>
2019-05-15 10:29:15 -0700

    Modify Fixutres for Clarity- Test #distance_between_pins for Pin Model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,10 @@
 class Pin < ApplicationRecord
   belongs_to :user
-  geocoded_by :ip_address, latitude: lat, longitude: lon
+  geocoded_by :ip
+  after_validation :geocode
+
+  def self.distance_between_pins user_pin, existing_pin
+    distance = Geocoder::Calculations.distance_between(user_pin, existing_pin)
+    distance <= 1.0 ? true : false
+  end
 end
