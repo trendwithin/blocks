3af04914733d8ad560244a754001a6a494cb2273
Michael Becco <e4e6d4d5@gmail.com>
2019-05-16 09:09:08 -0700

    Add Simple View For Dev Check New Service/Decorator

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
 
+  get 'pins/pinned_locations'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
