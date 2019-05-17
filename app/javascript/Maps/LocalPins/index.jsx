b4b921b7f98144b34b760f12b794c37f7d879d76
Michael Becco <e4e6d4d5@gmail.com>
2019-05-17 13:08:37 -0700

    Build Structure For Google Maps using React

---
+++ app/javascript/Maps/LocalPins/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class MappedLocalPins extends React.Component {
+  render () {
+    return (
+      <div>Map</div>
+    );
+  }
+}
+
+export default MappedLocalPins;
---
+++ app/javascript/packs/local_pins_map.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MappedLocalPins from 'Maps/LocalPins'
+
+ReactDOM.render(
+  <MappedLocalPins />, document.getElementById('local-pins')
+);
