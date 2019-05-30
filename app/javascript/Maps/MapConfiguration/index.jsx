94ebc8cf9d035e45261bd05368bf19c441eceee3
Michael Becco <e4e6d4d5@gmail.com>
2019-05-30 11:30:16 -0700

    Add Button Component- Add Toggle Button To Map to Product Current Pin, Fetch Pins from API

--- app/javascript/Maps/MapConfiguration/index.jsx
+++ app/javascript/Maps/MapConfiguration/index.jsx
@@ -1,58 +1,16 @@
 import React from 'react';
 import { DeviceLocation } from '../DeviceLocation';
-import { places } from '../MapMarkers/MarkerList/list_test_data';
+import Button from '../Buttons/FlashFriendButton';
+import pins from '../apis/FlashFriendEndPoint';
 
 export class MapConfiguration extends React.Component {
 
   state = {
+    map: {},
     currentLocation: {
       lat: 90.00000,
       lng: 135.0000,
     },
-  };
-
-  renderMap = () => {
-    const apiKey = process.env.GOOGLE_MAPS_API_KEY;
-    loadScript(`https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`);
-    window.initMap = this.initMap;
-  };
-
-  initMap = () => {
-    let map = new window.google.maps.Map(document.getElementById('map'), {
-      center: { lat: this.state.currentLocation.lat, lng: this.state.currentLocation.lng },
-      zoom: 16,
-    });
-
-    let infoWindow = new window.google.maps.InfoWindow();
-    const location = this.state.currentLocation;
-    places.markers.map(data => {
-      let contentString = `${data.name}`;
-
-      let marker = new window.google.maps.Marker(
-        { position: { lat: data.lat, lng: data.lng },
-        map: map,
-        title: data.name,
-      });
-
-      marker.addListener('click', function () {
-        infoWindow.setContent(contentString);
-        infoWindow.open(map, marker);
-      });
-    });
-
-    let current = new window.google.maps.Marker(
-      {
-        position: { lat: location.lat, lng: location.lng },
-        map: map,
-        title: 'Current Location',
-        icon: { url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png' },
-      }
-    );
-
-    current.addListener('click', function () {
-      infoWindow.setContent('Current Location');
-      infoWindow.open(map, current);
-    });
   };
 
   componentDidMount() {
@@ -66,20 +24,78 @@ export class MapConfiguration extends React.Component {
         });
       });
     }
-
     this.renderMap();
   }
 
-  componentDidUPdate(prevProps, prevState) {
+  componentDidUpdate(prevProps, prevState) {
     if (prevState.currentLocation !== this.state.currentLocation) {
-      // TBD
+      this.setState({ map: map });
     }
   }
+
+  handleButtonClick = async(toggled) => {
+    if (toggled) {
+      const lat = this.state.currentLocation.lat;
+      const lng = this.state.currentLocation.lng;
+      let infoWindow = new window.google.maps.InfoWindow();
+      if(toggled.clickedButton === 'Pin My Location') {
+        let marker = new window.google.maps.Marker(
+          { position: { lat: lat, lng: lng },
+          map: this.state.map,
+          title: 'Current Location',
+        });
+
+        marker.addListener('click', function () {
+          infoWindow.setContent('Current Location');
+          infoWindow.open(map, marker);
+        });
+      }
+
+      if(toggled.clickedButton === 'Find Local Pins') {
+        let infoWindow = new window.google.maps.InfoWindow();
+        const response = await pins.get('/pins');
+        const responseData = response.data;
+        responseData.data.map(data => {
+
+          let contentString = 'Make Dynamic Content String';
+
+          let marker = new window.google.maps.Marker(
+            { position: { lat: data.attributes.latitude, lng: data.attributes.longitude },
+            map: this.state.map,
+            title: data.name,
+          });
+
+          marker.addListener('click', function () {
+            infoWindow.setContent(contentString);
+            infoWindow.open(map, marker);
+          });
+        });
+      }
+    }
+  }
+
+  renderMap = () => {
+    const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+    loadScript(`https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`);
+    window.initMap = this.initMap;
+  };
+
+  initMap = () => {
+    let map = new window.google.maps.Map(document.getElementById('map'), {
+      center: { lat: this.state.currentLocation.lat, lng: this.state.currentLocation.lng },
+      zoom: 16,
+    });
+
+    this.setState({ map: map });
+  };
+
 
   render() {
     return (
       <main>
         <div id='map'></div>
+        <Button onButtonClick={this.handleButtonClick} text={'Pin My Location'}/>
+        <Button onButtonClick={this.handleButtonClick} text={'Find Local Pins'}/>
       </main>
     );
   }
