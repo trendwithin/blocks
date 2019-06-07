2c4c38e9d4909ac6625dea8f8922f9713c0903fd
Michael Becco <e4e6d4d5@gmail.com>
2019-06-07 12:10:52 -0700

    Add Topic Pin Relationship

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,4 @@
 class Topic < ApplicationRecord
   belongs_to :interest
+  has_many :pins
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_150020) do
+ActiveRecord::Schema.define(version: 2019_06_06_180943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2019_06_05_150020) do
     t.float "longitude", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "topic_id", null: false
+    t.index ["topic_id"], name: "index_pins_on_topic_id"
     t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
@@ -52,6 +54,7 @@ ActiveRecord::Schema.define(version: 2019_06_05_150020) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pins", "topics"
   add_foreign_key "pins", "users"
   add_foreign_key "topics", "interests"
 end
