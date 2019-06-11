87265ccff3f1cc4b386a2fba5fa516cd0f53b326
Michael Becco <e4e6d4d5@gmail.com>
2019-06-11 08:10:39 -0700

    Add Dynamic Drop Down Menu Component

--- app/javascript/Forms/CreatePin/index.jsx
+++ app/javascript/Forms/CreatePin/index.jsx
@@ -1,33 +1,31 @@
 import React from 'react';
-import FormOptions from './Options';
 import interests from '../apis/VersionOne';
 
 class CreatePinForm extends React.Component {
   state = {
     interests: [],
-    options: [],
+    topics: [],
     value: '',
-    toggleTopic: 'toggle-topic-field'
+    toggleTopic: 'toggle-topic-field',
   };
 
   onSelectChange = (event) => {
     this.setState({ value: event.target.value });
     this.setState({ toggleTopic: 'toggled-topic-field-visible' });
-    console.log('Changed');
-    console.log(event.target.value);
-  }
+    interests.get(`topics/interest_topics?interest=${event.target.value}`)
+      .then(response => {
+        let dataFromApi = response.data.data.map
+          (topic => { return { value: topic.id, display: topic.attributes.subject }; });
+        this.setState({ topics: dataFromApi });
+      });
+  };
 
   componentDidMount() {
     interests.get('/interests', { data: 'data' })
       .then(response => {
-        console.log(response);
-        this.setState({ interests: response.data.data });
-        return response;
-      })
-      .then(response => {
         let dataFromApi = response.data.data.map
           (interest => { return { value: interest.id, display: interest.attributes.curiosity }; });
-        this.setState({ options: dataFromApi });
+        this.setState({ interests: dataFromApi });
       });
   };
 
@@ -38,13 +36,15 @@ class CreatePinForm extends React.Component {
         <label htmlFor='pin_interest_id'>Interest</label>
         <select name="pin[interst_id]" id="pin_interest_id" onChange={this.onSelectChange} value={this.state.value}>
           <option value></option>
-          {this.state.options.map((item) => <option key={item.value} value={item.value}>{item.display}</option>)}
+          {this.state.interests.map((item) => <option key={item.value} value={item.display}>{item.display}</option>)}
         </select>
       </div>
       <div className='field' className={this.state.toggleTopic}>
         <label htmlFor='pin_topic_id'>Topic</label>
         <select name="pin[topic_id]" id="pin_topic_id">
           <option value></option>
+          {this.state.topics.map((item) => <option key={item.value} value={item.display}>{item.display}</option>)}
+
         </select>
       </div>
       </div>
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
       resources :users #, only: [:index]
       resources :pins
       resources :interests, only: [:index]
+      get 'topics/interest_topics'
     end
   end
 
