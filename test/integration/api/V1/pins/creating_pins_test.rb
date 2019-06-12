1a6757d78d5ed744db3e355182d086ca3a7074f5
Michael Becco <e4e6d4d5@gmail.com>
2019-06-12 10:55:57 -0700

    Add Unhappy Path to Test

--- test/integration/api/V1/pins/creating_pins_test.rb
+++ test/integration/api/V1/pins/creating_pins_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class CreatingPinsTest < ActionDispatch::IntegrationTest
-  test 'creates a pin' do
+  test 'creates a pin with valid data' do
     latitude = 47.623154
     longitude = -122.322318
     pin_attributes = {
@@ -14,5 +14,20 @@ class CreatingPinsTest < ActionDispatch::IntegrationTest
       post api_v1_pins_url, params: { pin: pin_attributes }, as: :json
     end
     assert_response :created
+  end
+
+  test 'resonse status when unprocessable entity' do
+    latitude = 47.623154
+    longitude = -122.322318
+    pin_attributes = {
+        latitude: latitude,
+        longitude: longitude,
+        user_id: users(:user_vic).id
+    }
+
+    assert_difference('Pin.count', 0) do
+      post api_v1_pins_url, params: { pin: pin_attributes }, as: :json
+    end
+    assert_response :unprocessable_entity
   end
 end
