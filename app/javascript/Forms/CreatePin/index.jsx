3d2073bb4c9e814234dea811b48d0b1f21eda897
Michael Becco <e4e6d4d5@gmail.com>
2019-06-15 14:39:31 -0700

    Wire Up Map With Form- Refactor Components- Sepearate out Unrelated Concerns into Own Grouping

--- app/javascript/Forms/CreatePin/index.jsx
+++ app/javascript/Forms/CreatePin/index.jsx
@@ -1,35 +1,45 @@
 import React from 'react';
-import interests from '../apis/VersionOne';
+import axiosApi from '../apis/VersionOne';
+import Button from '../../Maps/Buttons/FlashFriendButton';
 
 class CreatePinForm extends React.Component {
+  constructor(props) {
+    super(props);
+  };
 
   state = {
     interests: [],
     topics: [],
-    value: '',
-    topicValue: '',
-    toggleTopic: 'toggle-topic-field',
+    topicId: 0,
   };
 
-  onSelectChange = (event) => {
-    this.setState({ value: event.target.value });
-    this.setState({ toggleTopic: 'toggled-topic-field-visible' });
-    interests.get(`topics/interest_topics?interest=${event.target.value}`)
+
+
+  handleButtonClick = async(event) => {
+    event.preventDefault();
+    if (event.target.value === 'findLocalPinsButton') {
+      const topicId = this.state.topicId
+      await axiosApi.get(`/pins?topic=${topicId}`, { data: 'data' })
       .then(response => {
-        let dataFromApi = response.data.data.map
-          (topic => { return { value: topic.id, display: topic.attributes.subject }; });
-        this.setState({ topics: dataFromApi });
+        this.props.localPins(response.data.data);
+        // const userClicked = this.state.findLocalPinsClicked = !this.state.findLocalPinsClicked;
+        // this.setState(
+        //   { flashFriends: response.data.data,
+        //     findLocalPinsClicked: userClicked,
+        //   }
+        // );
+        // this.mapMarkers(response.data.data);
+      })
+      .catch(error => {
+        console.log(error);
       });
-  };
-
-  onTopicChange = (event) => {
-    // this.props.topics(topic);
-    this.setState({ topicValue: event.target.value });
-    this.props.topic(event.target.value);
-  };
+    } else if (event.target.value === 'pinLocationButton') {
+      this.props.pinLocation();
+    }
+  }
 
   componentDidMount() {
-    interests.get('/interests', { data: 'data' })
+    axiosApi.get('/interests', { data: 'data' })
       .then(response => {
         let dataFromApi = response.data.data.map
           (interest => { return { value: interest.id, display: interest.attributes.curiosity }; });
@@ -37,29 +47,67 @@ class CreatePinForm extends React.Component {
       });
   };
 
+  onInterestChange = () => {
+    this.props.showTopic();
+    this.onInterestChosen();
+  }
+
+  onInterestChosen = async () => {
+    await axiosApi.get(`topics/interest_topics?interest=${event.target.value}`)
+      .then(response => {
+        let dataFromApi = response.data.data.map
+          (topic => { return { value: topic.id, display: topic.attributes.subject }; });
+        this.setState({ topics: dataFromApi });
+    });
+  }
+
+  onTopicChange = (event) => {
+    this.setState({ topicId: event.target.value });
+    this.props.showButtons();
+  }
+
   render() {
     return (
+      <div className='flex-flash-form-container'>
       <div className='flex flex-row flex-wrap'>
         <div className='field flex flex-column w-50'>
           <label htmlFor='pin_interest_id'>Interest</label>
-          <select name="pin[interst_id]" id="pin_interest_id" onChange={this.onSelectChange} value={this.state.value}>
+          <select name="pin[interst_id]" id="pin_interest_id"
+            onChange={this.onInterestChange}
+          >
             <option value>Choose an Interest</option>
-            {this.state.interests.map((item) => <option key={item.value} value={item.display}>{item.display}</option>)}
+            { this.state.interests.map((item) =>
+              <option key={item.value} value={item.display}>{item.display}</option>)
+            }
           </select>
         </div>
         <div className='w-50'>
-          <div className={this.state.toggleTopic}>
+          <div className={this.props.hiddenTopic}>
             <div className='field flex flex-column'>
               <label htmlFor='pin_topic_id'>Topic</label>
-              <select name="pin[topic_id]" id="pin_topic_id" onChange={this.onTopicChange} value={this.state.topicValue}>
+              <select name="pin[topic_id]" id="pin_topic_id"
+                onChange={this.onTopicChange}
+              >
                 <option value>Choose a Topic</option>
-                {this.state.topics.map((item) => <option key={item.value} value={item.display}>{item.display}</option>)}
-
+                { this.state.topics.map((item) =>
+                    <option key={item.value} value={item.value}>{item.display}</option>)
+                }
               </select>
             </div>
           </div>
         </div>
+        <div className='field flex flex-column w-50'>
+          <div className={this.props.hiddenPinLocation}>
+            <button onClick={this.handleButtonClick} value='pinLocationButton'>Pin Location</button>
+          </div>
+        </div>
+        <div className='field flex flex-column w-50'>
+          <div className={this.props.hiddenPinLocation}>
+            <button onClick={this.handleButtonClick} value='findLocalPinsButton'>Find Local Pins</button>
+          </div>
+        </div>
       </div>
+    </div>
     );
   }
 };
--- app/javascript/Maps/Container/index.jsx
+++ app/javascript/Maps/Container/index.jsx
@@ -1,27 +1,74 @@
 import React from 'react';
 import MapConfiguration from '../MapConfiguration';
 import CreatePinForm from 'Forms/CreatePin';
+import FlashFriendList from '../Lists/FlashFriend/FriendList';
 
 class MapContainer extends React.Component {
   state = {
-    interestTopicToggled: false,
-    topicValue: '',
-  }
-
-  onInterestTopicToggled = () => {
-    this.setState({ interestTopicToggled: true });
+    currentLocation: {
+      lat: 90.00000,
+      lng: 135.0000,
+    },
+    topicValue: 0,
+    css: {
+      isHidden: true,
+      isHiddenTopic: 'toggle-display-none',
+      class: 'toggle-display-none',
+    },
+    pins: [],
+    pinLocation: false,
+    flashFriends: [],
   };
 
-  setTopicValue = (topic) => {
-    console.log(topic);
-    this.setState({ topicValue: topic });
+  setConfig = () => {
+    return '<MapConfiguration selectedTopic={this.state.topicValue}/>';
+  };
+
+  onInterestSelectedShowTopic = () => {
+    this.setState({ css: Object.assign({}, this.state.css, { class: 'toggle' })});
+  };
+
+  onTopicSelectedShowButtons = () => {
+    this.setState({ css: Object.assign({}, this.state.css, { isHiddenTopic: 'toggle' })});
+  };
+
+  onTopicSelectedShowPins = () => {
+    console.log('pinned');
+  };
+
+  findLocalPins = (pins) => {
+    this.setState({ pins: pins });
+    this.setState({ flashFriends: pins });
+  };
+
+  setCurrentLocation = (lat, lng) => {
+    this.setState({ currentLocation: Object.assign({}, this.state.currentLocation, { lat: lat, lng: lng })});
+  };
+
+  pinMyLocation = () => {
+    this.setState({ pinLocation: true });
   };
 
   render() {
     return (
       <div>
-        <MapConfiguration />
-        <CreatePinForm toggled={this.onInterestTopicToggled} topic={this.setTopicValue} />
+      <MapConfiguration
+        selectedTopic={this.state.topicValue}
+        updateCoordinates={this.setCurrentLocation}
+        pins={this.state.pins}
+        pinned={this.state.pinLocation}
+      />
+        <CreatePinForm
+          hiddenTopic={this.state.css.class}
+          showTopic={this.onInterestSelectedShowTopic}
+          hiddenPinLocation={this.state.css.isHiddenTopic}
+          showButtons={this.onTopicSelectedShowButtons}
+          localPins={this.findLocalPins}
+          pinLocation={this.pinMyLocation}
+        />
+        <FlashFriendList list={this.state.flashFriends}
+          userClicked={this.state.findLocalPinsClicked}
+        />
       </div>
     );
   }
