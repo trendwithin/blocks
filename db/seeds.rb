de6ea6b05a5bfcac45b73318283a00738c8a9653
Michael Becco <e4e6d4d5@gmail.com>
2019-07-09 17:34:49 -0700

    Modify Seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(email: 'test@example.com', password: 'password')
-user = User.create!(email: 'seed@example.com', password: 'password')
+Pin.delete_all
+Topic.delete_all
+Interest.delete_all
+User.delete_all
+
+user_one = User.create!(email: 'test@example.com', password: 'password')
+user_two = User.create!(email: 'seed@example.com', password: 'password')
 interest = Interest.create!(curiosity: 'Language')
 topic = Topic.create!(subject: 'Italian', interest_id: interest.id)
-pin = Pin.create(user_id: user.id, latitude: 47.599696, longitude: -122.332720, topic_id: topic.id)
+pin = Pin.create(user_id: user_one.id, latitude: 47.599696, longitude: -122.332720, topic_id: topic.id)
 
 webdev = Interest.create!(curiosity: 'Web Development')
 Topic.create!(subject: 'Rails', interest_id: webdev.id )
 Topic.create!(subject: 'React', interest_id: webdev.id )
+
+pin_message_one = PinMessage.new(user_id: user_one.id, pin_id: pin.id, message: 'At a Local Cafe')
+pin_message_one.save
+pin_message_two = PinMessage.new(user_id: user_two.id, pin_id: pin.id, message: 'I am near by')
+pin_message_two.save
