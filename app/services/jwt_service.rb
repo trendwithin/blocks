89bbd2ad93042f128380df334ac385a52abaaa18
Michael Becco <e4e6d4d5@gmail.com>
2019-08-13 17:31:06 -0700

    Add JWt Service to Create Token

--- app/services/jwt_service.rb
+++ app/services/jwt_service.rb
@@ -1,16 +1,14 @@
 class JwtService
 
-  def self.encode(payload, expiration = nil)
+  def self.encode(payload, expiration = 1)
+    @expiration ||= expiration
     payload = payload.dup
-    payload[:exp] = expiration.to_i.hours.from_now.to_i
+    payload[:exp] = @expiration.to_i.days.from_now.to_i
     JWT.encode(payload, Rails.application.secrets.secret_key_base, 'HS256')
   end
 
   def self.decode(token)
-    body, = JWT.decode(token, Rails.application.secrets.secret_key_base,
-                       true, algorithm: 'HS256')
-    HashWithIndifferentAccess.new(body)
-   rescue JWT::ExpiredSignature
-     nil
+    decoded_token = JWT.decode token, Rails.application.secrets.secret_key_base, true, algorithm: 'HS256'
+    decoded_token.first
   end
 end
