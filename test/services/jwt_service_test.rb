ee1fbe3aa0114b92085925d233ce0ef6f964b4b0
Michael Becco <e4e6d4d5@gmail.com>
2019-09-17 11:25:57 -0700

    Update Test To Mirror Current State

--- test/services/jwt_service_test.rb
+++ test/services/jwt_service_test.rb
@@ -6,15 +6,22 @@ class JwtServiceTest < ActiveSupport::TestCase
     @payload = { 'test' => 'payload' }
   end
 
-  test 'default .encode .decode cycle returns nil'  do
-    token = JwtService.encode(@payload)
-    decoded = JwtService.decode(token)
-    assert_nil decoded
+  test 'default .encode .decode without explicit expiration'  do
+    travel_to Time.zone.local(2019, 9, 14, 01, 04, 44) do
+      token = JwtService.encode(@payload)
+      decoded = JwtService.decode(token)
+      expected = { "test"=>"payload", "exp"=>1568509484 }
+      assert_equal expected,  decoded
+    end
+
   end
 
-  test '.encode .decode cycle with expiration arg' do
-    token = JwtService.encode(@payload, expiration = 7)
-    decoded = JwtService.decode(token)
-    assert_equal @payload, decoded
+  test '.encode .decode cycle with expplicit expiration arg' do
+    travel_to Time.zone.local(2019, 9, 14, 01, 04, 44) do
+      token = JwtService.encode(@payload, expiration = 7)
+      decoded = JwtService.decode(token)
+      expected = { "test"=>"payload", "exp"=>1568509484 }
+      assert_equal expected,  decoded
+    end
   end
 end
